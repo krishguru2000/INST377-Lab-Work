ab79c7a52ae3685610698921ee84ae0780315482
Krishnan Guruvayurappan <55109333+krishguru2000@users.noreply.github.com>
2023-03-11 18:35:20 -0500

    all_changes

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -3,9 +3,21 @@
   by adding `<script src="script.js">` just before your closing `</body>` tag
 */
 
+function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  })
+}
+
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  const filterButton = document.querySelector('.filter_button');
+  let currentList = [];
+
   form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+    
     submitEvent.preventDefault(); // This prevents your page from going to http://localhost:3000/api even if your form still has an action set on it
     console.log('form submission'); // this is substituting for a "breakpoint"
 
@@ -36,6 +48,11 @@ async function mainEvent() { // the async keyword means we can make API requests
     */
 
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    console.log(results);
+
+    currentList = await results.json();
+    console.table(currentList);
+
     /*
    ## Get request with query parameters
 
@@ -56,6 +73,16 @@ async function mainEvent() { // the async keyword means we can make API requests
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
     // it initially contains all 1,000 records from your request
   });
+  filterButton.addEventListener('click', (event) => {
+    console.log('clicked Filterbutton');
+
+    const formData = new FormData(form);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+    console.log(newList)
+  })
 }
 
 /*
@@ -63,4 +90,7 @@ async function mainEvent() { // the async keyword means we can make API requests
   The use of the async keyword means we can "await" events before continuing in our scripts
   In this case, we load some data when the form has submitted
 */
+
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+
+
